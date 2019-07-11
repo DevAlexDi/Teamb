abaae071ebdc47569caaff6a8fd5caa46fa00f2d
keitmark <keitmark@mail.ru>
2019-07-11 09:58:06 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    // $('.tel-inp').inputmask({
+    //     "mask": "+7 (999) 999-99-99"
+    //     , "placeholder": "_"
+    //     , showMaskOnHover: false
+    //     , showMaskOnFocus: true
+    // });
+
+    
+
+});
+
+
